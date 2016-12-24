385a65d96ceabaa777af14956cd93c8d4e256555
Lola Adellia <lolaadellia@gmail.com>
2016-12-24 09:10:02 +0700

    SQLite

---
+++ app/src/main/java/com/lolaadellia/meruvian/content/database/model/NewsDatabaseModel.java
@@ -0,0 +1,15 @@
+package com.lolaadellia.meruvian.content.database.model;
+
+/**
+ * Created by Hanum on 24/12/2016.
+ */
+
+public class NewsDatabaseModel {
+
+    public static final String ID = "id";
+    public static final String TITLE = "title";
+    public static final String CONTENT = "content";
+    public static final String STATUS = "status";
+    public static final String CREATE_DATE = "create_date";
+}
+
